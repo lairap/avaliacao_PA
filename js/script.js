255b4f79d5ab06a6914ad14533d45ed90138ac3c
lairap <lairaluizapires@gmail.com>
2024-04-16 21:20:11 -0300

    quarto commit, navbar, overflow

--- js/script.js
+++ js/script.js
@@ -4,4 +4,4 @@ const sidebar = document.querySelector('.container .sidebar');
 
 menuOpen.addEventListener('click', () => sidebar.style.left = '0');
 
-menuClose.addEventListener('click', () => sidebar.style.left = '-100%');
+menuClose.addEventListener('click', () => sidebar.style.left = '-100%'); 
